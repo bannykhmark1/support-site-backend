ba5d3925a65c02e6d8f481381912dc9dbd87c073
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-22 14:48:00 +0500

    do websocket connetct

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const http = require('http'); // Для WebSocket
+const WebSocket = require('ws'); // Для WebSocket
 const sequelize = require('./db');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
@@ -8,12 +10,30 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const router = require('./routes/index');
 const userRouter = require('./routes/userRouter');
 const nodemailer = require('nodemailer');
-const announcememntController = require('./controllers/announcememntController');
-const announcememntRouter = require('./routes/announcementRouter')
+const announcememntRouter = require('./routes/announcementRouter');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const server = http.createServer(app); // Создаем сервер HTTP
+const wss = new WebSocket.Server({ server }); // Создаем WebSocket сервер
+
+// WebSocket обработчики
+wss.on('connection', (ws) => {
+    console.log('New WebSocket connection');
+
+    ws.on('message', (message) => {
+        console.log('received: %s', message);
+        // Пример ответа на сообщение
+        ws.send(`Received your message: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket connection closed');
+    });
+
+    ws.send('Welcome to the WebSocket server');
+});
 
 app.use(cors());
 app.use(express.json());
@@ -49,21 +69,19 @@ app.post('/send', async (req, res) => {
     }
 });
 
-// Mount routers
+// Маршруты API
 app.use('/api', router);
-
 app.use('/api/user', userRouter);
-
 app.use('/api/announcements', announcememntRouter);
 
-// Error handling middleware должен быть вызван в самом конце
+// Обработка ошибок
 app.use(errorHandler);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.error(e);
   }
