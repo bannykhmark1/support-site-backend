2664405cde333ec141d9e6f64326fe64c6e6366c
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-07-29 17:40:50 +0500

    asdasd

--- models/models.js
+++ models/models.js
@@ -7,7 +7,7 @@ const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.STRING, defaultValue: "ADMIN" },
+    role: { type: DataTypes.STRING, defaultValue: "USER" },
     name: { type: DataTypes.STRING, allowNull: false } // Новое поле "name"
 });
 
