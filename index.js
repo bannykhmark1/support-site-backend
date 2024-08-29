0afea58113a9507cc06d59c84cb33abfe38febe5
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2024-08-29 18:55:01 +0500

    asdasd

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); // Для HTTP-запросов
+const axios = require('axios');
 const sequelize = require('./db');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const path = require('path');
-const session = require('express-session'); // Для работы с сессиями
+const session = require('express-session');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const router = require('./routes/index');
 const userRouter = require('./routes/userRouter');
 const announcementRouter = require('./routes/announcementRouter');
+const http = require('http'); // Для создания HTTP сервера
+const WebSocket = require('ws'); // Подключаем WebSocket
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,16 +19,17 @@ const app = express();
 
 // Настройка сессий
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'your-secret-key', // Убедитесь, что SESSION_SECRET установлен в переменных окружения
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // Для разработки используйте secure: false, для продакшн среды установите true
+  cookie: { secure: false }
 }));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({ createParentPath: true }));
+
 // Маршруты API
 app.use('/api', router);
 app.use('/api/user', userRouter);
@@ -35,11 +38,30 @@ app.use('/api/announcements', announcementRouter);
 // Обработка ошибок
 app.use(errorHandler);
 
+// Создание HTTP сервера и интеграция с WebSocket
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Настройка WebSocket
+wss.on('connection', (ws) => {
+    console.log('Новое WebSocket подключение');
+
+    ws.on('message', (message) => {
+        console.log('Получено сообщение:', message);
+        // Отправляем ответ
+        ws.send('Привет от сервера!');
+    });
+
+    ws.on('close', () => {
+        console.log('Соединение закрыто');
+    });
+});
+
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.error(e);
     }
